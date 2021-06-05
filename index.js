c9ff52a8d0781d7b73e014cb97336c011c8b4bc3
koalakoker <koalakoker@gmail.com>
2021-06-05 22:00:38 +0200

    Hello world express

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+const express = require('express');
+const PORT = process.env.PORT || 5000
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
