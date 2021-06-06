e09d0b93a9379e70f95a283b006a5f7d34f9cc19
koalakoker <koalakoker@gmail.com>
2021-06-06 07:15:55 +0200

    Basic get

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+let links = [
+  {
+    "name": "koalakoker home page",
+    "url": "www.koalakoker.com"
+  },
+  {
+    "name": "Google",
+    "url": "www.google.it"
+  }]
+
+app.get('/api/links', (req, res) => {
+  res.set('Content-Type', 'application/json');
+  res.send(JSON.stringify(links));
+})
+
+app.put('/api/links/:id', (req, res) => {
+  res.send("Put");
 })
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
