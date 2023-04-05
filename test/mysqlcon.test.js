18f2832571340b1c17f7aa30b140678044b4f64c
Gianluigi Forte <koalakoker@gmail.com>
2023-04-05 08:17:29 +0200

    Fixed mysqlcon test clearing inserted rows

--- test/mysqlcon.test.js
+++ test/mysqlcon.test.js
@@ -3,49 +3,58 @@ import { Element } from "../src/connection/element.js";
 import { getRandomName } from "../src/utils/lib.js";
 import { assert } from "chai";
 
+const table = "drawings";
+
 describe("MySQLcon tests", function () {
   const con = createMySQLConnection();
   it("CURD tests", async function () {
     await CURDtest(con);
   });
   it("Select all from user x", async function () {
-    let data = getNewData();
-    const user = data.at(0);
+    let rData = getNewData();
+    const user = rData.at(0);
 
+    const insertedItems = [];
     for (let i = 0; i < 10; i++) {
-      data = getNewData();
+      rData = getNewData();
       if (i % 2 == 0) {
-        data[0] = user;
+        rData[0] = user;
       }
-      await con.create("drawings", data);
+      const row = await con.create(table, rData);
+      insertedItems.push(row.id);
     }
 
-    const results = await con.filterBy("drawings", user.col, user.value);
+    const results = await con.filterBy(table, user.col, user.value);
     assert.equal(results.length, 5);
+
+    insertedItems.forEach(async (id) => {
+      await con.delete(table, id);
+    });
+
     con.end();
   });
 });
 
 async function CURDtest(con) {
   let data = getNewData();
-  let getElement = await con.create("drawings", data);
+  let getElement = await con.create(table, data);
   const id = getElement.id;
   assert.equal(getElement.user, data.at(0).value);
   assert.equal(getElement.name, data.at(1).value);
   assert.equal(getElement.drawing, data.at(2).value);
 
   data = getNewData();
-  await con.update("drawings", id, data);
+  await con.update(table, id, data);
 
-  getElement = await con.get("drawings", id);
+  getElement = await con.get(table, id);
   assert.equal(getElement.length, 1);
   assert.equal(getElement[0].user, data.at(0).value);
   assert.equal(getElement[0].name, data.at(1).value);
   assert.equal(getElement[0].drawing, data.at(2).value);
 
-  await con.delete("drawings", id);
+  await con.delete(table, id);
 
-  getElement = await con.get("drawings", id);
+  getElement = await con.get(table, id);
   assert.equal(getElement.length, 0);
 }
 
