d989c6e86ce287fb746ca055b4f9b40e8cdfbae5
Gianluigi Forte <koalakoker@gmail.com>
2023-04-11 05:38:07 +0200

    Implemented getNextId

--- src/connection/getNextId.js
+++ src/connection/getNextId.js
@@ -1,15 +1,10 @@
 import { createMySQLConnection } from "./mysqlCon.js";
 
-export async function getNextId() {
-  const con = createMySQLConnection();
-  await con.getNextId("drawings");
-  con.end();
-}
-
 const con = createMySQLConnection();
 con.getNextId("drawings").then((nextID) => {
-  console.log("Previous value: " + nextID);
-  con.setNextId("drawings", nextID + 10).then(() => {
-    con.end();
-  });
+  console.log("nextID: " + nextID);
+  con.end();
+  // con.setNextId("drawings", nextID + 10).then(() => {
+  //   con.end();
+  // });
 });
